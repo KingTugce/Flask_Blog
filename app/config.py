2907c9bf98e9b217d021a1bb44560f2bbc76c14c
KingTugce <kingstugce@gmail.com>
2023-03-11 12:02:37 -0800

    initial commit

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__name__))
+#  /Users/tugcesimsek/Desktop/flask_blog/app/config.py
+
+class Config:
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+    SQLALCHEMY_DATABASE_MODIFICATIONS = False
